5f310465ddd0054c9a4567ef02b624909d7d1b41
hy2967 <hy2967@gmail.com>
2020-05-15 00:53:37 +0900

    Login page 2 - make login page (front)

--- src/main/java/com/memoweb/web/LoginController.java
+++ src/main/java/com/memoweb/web/LoginController.java
@@ -2,6 +2,7 @@ package com.memoweb.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +14,14 @@ public class LoginController {
     public ModelAndView loginGet() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @PostMapping("/memo/login")
+    public ModelAndView loginPost() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("");
+
         return modelAndView;
     }
 }
